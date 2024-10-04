9972f281b97fe225af7d6dddb65792890990d6d7
akint980 <akintunde_aderonke@yahoo.com>
2024-10-04 14:26:15 -0500

    update index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function initMap() {
   const park = { lat: 30.196033, lng: -95.199577};
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 8,
     center: park,
   });
   const contentString =
@@ -13,15 +13,14 @@ function initMap() {
     '<h1 id="firstHeading" class="firstHeading">Big Rivers waterpark and Adventure</h1>' +
     '<div id="bodyContent">' +
     "<p><b>Big Rivers waterpark and Adventure</b>, also referred to as <b>Park</b>, is a large " +
-    "waterpark in the central part of the " +
-    "Texas. It lies 335&#160;km (208&#160;mi) " +
-    "south west of the nearest large town, Alice Springs; 450&#160;km " +
-    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-    "Aboriginal people of the area. It has many springs, waterholes, " +
-    "rock caves and ancient paintings. Uluru is listed as a World " +
-    "Site.</p>" +
+    "waterpark in the central part of the Texas" +
+    "It is located in Waco, Texas. The park lies " +
+    "near the Brazos River making it a scenic spot for water activities" +
+    "during vacations and holidays. Features include COLORado Racer, " +
+    "Wild Isle, Hawk's Glide and Eagle Challenge this is " +
+    "is a must vist place for relaxation. It has many springs, waterholes, " +
+    "attractions. This park is listed as a World " +
+    "must visit place.</p>" +
     '<p>Attribution: Park, <a href="https://bigriverswaterpark.com">' +
     "https://bigriverswaterpark.com/</a> " +
     "(fun place to visit).</p>" +
