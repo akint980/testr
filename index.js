d28134ae494bb1bd14b416e550442456ca048e5f
akint980 <akintunde_aderonke@yahoo.com>
2024-10-04 13:19:58 -0500

    update

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ function initMap() {
     '<div id="content">' +
     '<div id="siteNotice">' +
     "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<h1 id="firstHeading" class="firstHeading">Big Rivers waterpark and Advventure</h1>' +
     '<div id="bodyContent">' +
-    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-    "waterpark in the southern part of the " +
-    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+    "<p><b>Big Rivers waterpark and Advventure</b>, also referred to as <b>Aquq park</b>, is a large " +
+    "waterpark in the central part of the " +
+    "Texas. It lies 335&#160;km (208&#160;mi) " +
     "south west of the nearest large town, Alice Springs; 450&#160;km " +
     "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
     "features of the Uluru - Kata Tjuta National Park. Uluru is " +
     "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
     "Aboriginal people of the area. It has many springs, waterholes, " +
     "rock caves and ancient paintings. Uluru is listed as a World " +
-    "Heritage Site.</p>" +
+    "Site.</p>" +
     '<p>Attribution: Uluru, <a href="https://bigriverswaterpark.com">' +
     "https://bigriverswaterpark.com/</a> " +
-    "(last visited June 22, 2009).</p>" +
+    "(fun place to visit).</p>" +
     "</div>" +
     "</div>";
   const infowindow = new google.maps.InfoWindow({
