7ad8e2000dc9d91e213390d17b3a7af840739af1
akint980 <akintunde_aderonke@yahoo.com>
2024-10-04 13:51:33 -0500

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // This example displays a marker at the center of Australia.
 // When the user clicks the marker, an info window opens.
 function initMap() {
-  const Aqua = { lat: 31.5545, lng: 97.1326 };
+  const park = { lat: 31.5545, lng: 97.1326 };
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 8,
-    center: Aqua,
+    zoom: 4,
+    center: park,
   });
   const contentString =
     '<div id="content">' +
@@ -12,7 +12,7 @@ function initMap() {
     "</div>" +
     '<h1 id="firstHeading" class="firstHeading">Big River waterpark and Adventure</h1>' +
     '<div id="bodyContent">' +
-    "<p><b>Big Rivers waterpark and Advventure</b>, also referred to as <b>Aqua park</b>, is a large " +
+    "<p><b>Big Rivers waterpark and Advventure</b>, also referred to as <b>Park</b>, is a large " +
     "waterpark in the central part of the " +
     "Texas. It lies 335&#160;km (208&#160;mi) " +
     "south west of the nearest large town, Alice Springs; 450&#160;km " +
@@ -22,19 +22,19 @@ function initMap() {
     "Aboriginal people of the area. It has many springs, waterholes, " +
     "rock caves and ancient paintings. Uluru is listed as a World " +
     "Site.</p>" +
-    '<p>Attribution: Uluru, <a href="https://bigriverswaterpark.com">' +
+    '<p>Attribution: Park, <a href="https://bigriverswaterpark.com">' +
     "https://bigriverswaterpark.com/</a> " +
     "(fun place to visit).</p>" +
     "</div>" +
     "</div>";
   const infowindow = new google.maps.InfoWindow({
     content: contentString,
-    ariaLabel: "Uluru",
+    ariaLabel: "park",
   });
   const marker = new google.maps.Marker({
-    position: uluru,
+    position: park,
     map,
-    title: "Uluru (Ayers Rock)",
+    title: "Big River waterpark and Adventure (park)",
   });
 
   marker.addListener("click", () => {
